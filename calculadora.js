9dc3c5846d135c8924a9f1a44d1d2c818ce3f2ba
Comicion20FrancoGaspar <francogaspar.b.s@hotmail.com>
2023-05-16 20:01:17 -0300

    mi primer commit

---
+++ calculadora.js
@@ -0,0 +1,20 @@
+const sumar = function name(a,b) {
+    return a + b
+}
+
+const restar = function name(a,b) {
+    return a - b
+}
+
+const multiplicar = function name(a,b) {
+    return a* b
+}
+
+const dividir = function name(a,b) {
+    return a / b
+}
+
+module.exports = {
+    sumar, restar, multiplicar, dividir
+
+}
